d92ee8415f047c642c8265d0e8ca8906d157df80
catherine-sz <65593438+catherine-sz@users.noreply.github.com>
2020-07-24 08:56:51 +0800

    Update SAMSUNG UTAG.user.js

--- SAMSUNG UTAG.user.js
+++ SAMSUNG UTAG.user.js
@@ -4,12 +4,16 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       Catherine
-// @include       *catherineszeto.com/*
+// @include       *samsung.com/*
 
 // ==/UserScript==
+var check_value = sessionStorage['tealium_script'];
 
-(function(a,b,c,d){
-    a='//tags.tiqcdn.com/utag/samsung-gmo/khq/dev/utag.js';
-    b=document;c='script';d=b.createElement(c);d.src=a;d.type='text/java'+c;d.async=true;
-    a=b.getElementsByTagName(c)[0];a.parentNode.insertBefore(d,a);
-})();
+if (document.URL.indexOf("tealium=true") > -1 || check_value == "true"){
+    sessionStorage['tealium_script'] = "true";
+    (function(a,b,c,d){
+        a='//tags.tiqcdn.com/utag/samsung-gmo/khq/dev/utag.js';
+        b=document;c='script';d=b.createElement(c);d.src=a;d.type='text/java'+c;d.async=true;
+        a=b.getElementsByTagName(c)[0];a.parentNode.insertBefore(d,a);
+    })();
+}
